64040aee3396dda076359886981946ac18c6af8e
Chayse Summers <csummers233@southseattle.edu>
2015-05-07 12:14:59 -0700

    This is a brand new Rails project for developing a blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniProject::Application.config.secret_key_base = 'ab4979329c40d4d37e4ff396a07fe8569819bbfa1ccc2a06896e89475a4e9a174909e37391ef3b4346611bb71da9f4ebf34f4838a5a027d2cce64cbbb93775bf'
